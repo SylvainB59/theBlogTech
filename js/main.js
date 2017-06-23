dbe0e506994352f67784ed6133117a4d9ea6b97b
sylvain <s.baiilot59@gmail.com>
2017-06-23 14:36:20 +0200

    add js date, chrono, showAlt

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Date et heure
+
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+    var d = new Date();
+    document.getElementById("time").innerHTML = Date();
+}
+
+
+
+// chrono seconde
+
+function timer() {
+var h = document.getElementById("chrono").innerHTML;
+h++;
+document.getElementById("chrono").innerHTML = h;
+}
+setInterval("timer()", 1000);
+
+
+// view alt
+
+var myImage = document.getElementsByTagName("img");
+var text = document.getElementById("show");
+
+for (var i = 0; i < myImage.length; i++) {
+    myImage[i].addEventListener('mouseover',show);
+    myImage[i].addEventListener('mouseout',hide);
+}
+
+function show(){
+  var myAlt = this.alt;
+  text.innerHTML = myAlt;
+}
+
+function hide(){
+  var vide = "";
+  text.innerHTML = vide;
+}
